ef843fd7bae7ad9e70a2ec73e3bc9ef831851d12
lukepark327 <lukepark327@gmail.com>
2024-03-28 09:40:28 +0900

    feat: dropout

--- src/models.py
+++ src/models.py
@@ -66,22 +66,46 @@ class CNNFashion_Mnist(nn.Module):
         return out
 
 
+# class CNNCifar(nn.Module):
+#     def __init__(self, args):
+#         super(CNNCifar, self).__init__()
+#         self.conv1 = nn.Conv2d(3, 6, 5)
+#         self.pool = nn.MaxPool2d(2, 2)
+#         self.conv2 = nn.Conv2d(6, 16, 5)
+#         self.fc1 = nn.Linear(16 * 5 * 5, 120)
+#         self.fc2 = nn.Linear(120, 84)
+#         self.fc3 = nn.Linear(84, args.num_classes)
+
+#     def forward(self, x):
+#         x = self.pool(F.relu(self.conv1(x)))
+#         x = self.pool(F.relu(self.conv2(x)))
+#         x = x.view(-1, 16 * 5 * 5)
+#         x = F.relu(self.fc1(x))
+#         x = F.relu(self.fc2(x))
+#         x = self.fc3(x)
+#         return F.log_softmax(x, dim=1)
 class CNNCifar(nn.Module):
-    def __init__(self, args):
+    def __init__(self, args, dropout_rate=0.25):
         super(CNNCifar, self).__init__()
         self.conv1 = nn.Conv2d(3, 6, 5)
         self.pool = nn.MaxPool2d(2, 2)
         self.conv2 = nn.Conv2d(6, 16, 5)
+        self.dropout1 = nn.Dropout(dropout_rate)
         self.fc1 = nn.Linear(16 * 5 * 5, 120)
+        self.dropout2 = nn.Dropout(dropout_rate)
         self.fc2 = nn.Linear(120, 84)
+        self.dropout3 = nn.Dropout(dropout_rate)
         self.fc3 = nn.Linear(84, args.num_classes)
 
     def forward(self, x):
         x = self.pool(F.relu(self.conv1(x)))
         x = self.pool(F.relu(self.conv2(x)))
         x = x.view(-1, 16 * 5 * 5)
+        x = self.dropout1(x)
         x = F.relu(self.fc1(x))
+        x = self.dropout2(x)
         x = F.relu(self.fc2(x))
+        x = self.dropout3(x)
         x = self.fc3(x)
         return F.log_softmax(x, dim=1)
 

4e45cbcd57f02dffaaf33f370fa960e104af4819
lukepark327 <lukepark327@gmail.com>
2024-03-28 10:11:06 +0900

    chore: test settings

--- test.sh
+++ test.sh
@@ -8,4 +8,4 @@ python src/fedAsync_main.py --model=cnn --dataset=cifar --iid=1 --epochs=1000 --
 
 python src/brain_main.py --model=cnn --dataset=cifar --iid=1 --epochs=1000 --num_users=21 --byzantines=0 --score_byzantines=0 --frac=0.1 --stale=4 --diff=0.55 --window=4 --threshold=0.0 --verbose=0 --local_bs=50
 python src/brain_main.py --model=cnn --dataset=cifar --iid=1 --epochs=1000 --num_users=21 --byzantines=10 --score_byzantines=0 --frac=0.1 --stale=4 --diff=0.55 --window=4 --threshold=0.125 --verbose=0 --local_bs=50
-python src/brain_main.py --model=cnn --dataset=cifar --iid=1 --epochs=1000 --num_users=21 --byzantines=0 --score_byzantines=5 --frac=0.1 --stale=4 --diff=0.55 --window=4 --threshold=0.0 --verbose=0 --local_bs=50
+python src/brain_main.py --model=cnn --dataset=cifar --iid=1 --epochs=1000 --num_users=21 --byzantines=0 --score_byzantines=10 --frac=0.1 --stale=4 --diff=1.0 --window=4 --threshold=0.0 --verbose=0 --local_bs=50

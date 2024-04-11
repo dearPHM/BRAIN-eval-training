4a946695dc6e1995a33a29eca80c9e0b85c63cd4
lukepark327 <lukepark327@gmail.com>
2024-04-11 17:45:43 +0900

    chore: options

--- src/options.py
+++ src/options.py
@@ -8,12 +8,13 @@ import argparse
 def args_parser():
     parser = argparse.ArgumentParser()
 
-    # federated arguments (Notation for the arguments followed from paper)
-    parser.add_argument('--num_users', type=int, default=100,
-                        help="number of users: K")
-    parser.add_argument('--byzantines', type=int, default=49,
+    parser.add_argument('--byzantines', type=int, default=0,
                         help="number of Byzantine users who submit zero weights: Z")
-    parser.add_argument('--frac', type=float, default=0.1,
+
+    # federated arguments (Notation for the arguments followed from paper)
+    parser.add_argument('--num_users', type=int, default=21,
+                        help="number of users (nodes): K")
+    parser.add_argument('--frac', type=float, default=0.1,  # 10%
                         help='the fraction of clients: C')
 
     # fedasync arguments
@@ -24,43 +25,43 @@ def args_parser():
 
     # BRAIN arguments
     parser.add_argument('--diff', type=float, default=0.55,
-                        help='(inverse of) difficulty related to sortition (default: 0.55)')
+                        help='the franction related to quorum (default: 0.55 => 21*0.55=11 nodes)')
     parser.add_argument('--window', type=int, default=4,
                         help='window size for moving averaging (default: 4) (>= 2)')
     parser.add_argument('--threshold', type=float, default=0.125,
                         help='accuracy threshold to ignore (default: 0.125)')
-    parser.add_argument('--score_byzantines', type=int, default=49,
+    parser.add_argument('--score_byzantines', type=int, default=0,
                         help="number of Byzantine users who submit random score: SZ")
 
     # model arguments
     parser.add_argument('--model', type=str, default='cnn', help='model name')
     # parser.add_argument('--norm', type=str, default='batch_norm',
     #                     help="batch_norm, layer_norm, or None")
-
-    # other arguments
     parser.add_argument('--dataset', type=str, default='cifar', help="name \
                         of dataset")
     parser.add_argument('--num_classes', type=int, default=10, help="number \
                         of classes")
-    parser.add_argument('--gpu', type=int, default=None, help="To use cuda, set \
-                        to a specific GPU ID. Default set to use CPU.")
-    parser.add_argument('--epochs', type=int, default=10,  # 400
+    parser.add_argument('--lr', type=float, default=11.5,  # 9.0 for single SGD
+                        help='learning rate (default: 11.5)')
+
+    # other arguments
+    # parser.add_argument('--gpu', type=int, default=None, help="To use cuda, set \
+    # to a specific GPU ID. Default set to use CPU.")
+    parser.add_argument('--epochs', type=int, default=200,  # Rounds
                         help="number of rounds of training")
-    parser.add_argument('--local_ep', type=int, default=10,
+    parser.add_argument('--local_ep', type=float, default=9.9,  # 10
                         help="the number of local epochs: E")
-    parser.add_argument('--local_bs', type=int, default=1024,  # 50
+    parser.add_argument('--local_bs', type=int, default=1024,
                         help="local batch size: B")
-    parser.add_argument('--lr', type=float, default=0.001,
-                        help='learning rate')
-    parser.add_argument('--momentum', type=float, default=0.9,
-                        help='SGD momentum (default: 0.9)')
-    parser.add_argument('--optimizer', type=str, default='sgd', help="type \
-                        of optimizer")
+    # parser.add_argument('--momentum', type=float, default=0.9,
+    #                     help='SGD momentum (default: 0.9)')
+    # parser.add_argument('--optimizer', type=str, default='sgd', help="type \
+    #                     of optimizer")
     parser.add_argument('--iid', type=int, default=1,
-                        help='Default set to IID. Set to 0 for non-IID.')
-    parser.add_argument('--unequal', type=int, default=0,
-                        help='whether to use unequal data splits for  \
-                        non-i.i.d setting (use 0 for equal splits)')
+                        help='Default set to IID. Set to 0 for non-IID (and unequal).')
+    # parser.add_argument('--unequal', type=int, default=0,
+    #                     help='whether to use unequal data splits for  \
+    #                     non-i.i.d setting (use 0 for equal splits)')
     # parser.add_argument('--stopping_rounds', type=int, default=10,
     #                     help='rounds of early stopping')
     parser.add_argument('--verbose', type=int, default=1, help='verbose')

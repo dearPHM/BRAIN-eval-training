2f3078cf3d0c769ba940cdefbe99de4b3ad8bea7
lukepark327 <lukepark327@gmail.com>
2024-04-11 17:45:07 +0900

    feat: airbench training

--- src/update.py
+++ src/update.py
@@ -5,7 +5,12 @@
 import copy
 import torch
 from torch import nn
-from torch.utils.data import DataLoader, Dataset
+# from torchvision.datasets import CIFAR10
+# from torch.utils.data import DataLoader, Dataset
+from torch.utils.data import Dataset
+
+from airbench.dataloader import CifarLoader
+from airbench.model import train
 
 
 class DatasetSplit(Dataset):
@@ -16,24 +21,25 @@ class DatasetSplit(Dataset):
         self.dataset = dataset
         self.idxs = [int(i) for i in idxs]
 
+        self.classes = self.dataset.classes
+        self.data = dataset.data[self.idxs]
+        self.targets = [dataset.targets[i] for i in self.idxs]
+
     def __len__(self):
         return len(self.idxs)
 
     def __getitem__(self, item):
         image, label = self.dataset[self.idxs[item]]
-        return (image).clone().detach(), torch.tensor(label)
+        # return (image).clone().detach(), torch.tensor(label)
+        return image, label
 
 
 class LocalUpdate(object):
-    def __init__(self, args, dataset, idxs, logger):
+    def __init__(self, args, hyps, dataset, idxs, logger, gpu=0):
         self.args = args
+        self.hyps = hyps
         self.logger = logger
-        self.device = 'cuda' if args.gpu else 'cpu'
-
-        self.device = torch.device('cuda' if (
-            args.gpu != None and torch.cuda.is_available()) else 'cpu')
-        if self.device.type == 'cuda':
-            torch.cuda.set_device(args.gpu)
+        self.device = torch.device(gpu)
 
         self.trainloader, self.validloader, self.testloader = self.train_val_test(
             dataset, list(idxs))
@@ -48,53 +54,47 @@ class LocalUpdate(object):
         and user indexes.
         """
         # split indexes for train, validation, and test (80, 10, 10)
-        idxs_train = idxs[:int(0.8*len(idxs))]
-        idxs_val = idxs[int(0.8*len(idxs)):int(0.9*len(idxs))]
+        idxs_train = idxs[:int(0.9*len(idxs))]
+        # idxs_val = idxs[int(0.8*len(idxs)):int(0.9*len(idxs))]
         idxs_test = idxs[int(0.9*len(idxs)):]
 
-        trainloader = DataLoader(DatasetSplit(dataset, idxs_train),
-                                 batch_size=self.args.local_bs, num_workers=4, shuffle=True)
-        validloader = DataLoader(DatasetSplit(dataset, idxs_val),
-                                 batch_size=self.args.local_bs, num_workers=4, shuffle=False)
-        testloader = DataLoader(DatasetSplit(dataset, idxs_test),
-                                batch_size=self.args.local_bs, num_workers=4, shuffle=False)
-        return trainloader, validloader, testloader
+        trainloader = CifarLoader(DatasetSplit(dataset, idxs_train),
+                                  train=True, batch_size=self.args.local_bs, aug={'flip': True, 'translate': 2, })
+        # validloader = CifarLoader(DatasetSplit(dataset, idxs_val),
+        #  batch_size=self.args.local_bs)
+        testloader = CifarLoader(DatasetSplit(dataset, idxs_test),
+                                 train=False, batch_size=2000)
+        return trainloader, None, testloader
 
-    def update_weights(self, model, global_round):
-        # Set mode to train model
-        model.train()
-        epoch_loss = []
+    def update_weights(self, model, epochs=9.9, global_round=None):
+        # Set Params
+        batch_size = self.hyps['opt']['batch_size']
+        momentum = self.hyps['opt']['momentum']
+        # Assuming gradients are constant in time, for Nesterov momentum, the below ratio is how much
+        # larger the default steps will be than the underlying per-example gradients. We divide the
+        # learning rate by this ratio in order to ensure steps are the same scale as gradients, regardless
+        # of the choice of momentum.
+        kilostep_scale = 1024 * (1 + 1 / (1 - momentum))
+        # un-decoupled learning rate for PyTorch SGD
+        # lr = self.hyps['opt']['lr'] / kilostep_scale
+        lr = self.args.lr / kilostep_scale
+        wd = self.hyps['opt']['weight_decay'] * batch_size / kilostep_scale
+        lr_biases = lr * self.hyps['opt']['bias_scaler']
+        label_smoothing = self.hyps['opt']['label_smoothing']
+        whiten_bias_epochs = self.hyps['opt']['whiten_bias_epochs']
 
-        # Set optimizer for the local updates
-        if self.args.optimizer == 'sgd':
-            optimizer = torch.optim.SGD(model.parameters(), lr=self.args.lr,
-                                        momentum=self.args.momentum)
-        elif self.args.optimizer == 'adam':
-            optimizer = torch.optim.Adam(model.parameters(), lr=self.args.lr,
-                                         weight_decay=1e-4)
+        tta_level = self.hyps['net']['tta_level']
 
-        for iter in range(self.args.local_ep):
-            batch_loss = []
-            for batch_idx, (images, labels) in enumerate(self.trainloader):
-                images, labels = images.to(self.device), labels.to(self.device)
+        train_acc_collect, train_loss_collect, acc_collect = train(
+            # run,
+            model,
+            self.trainloader, self.testloader,
+            batch_size, epochs, momentum, lr, wd, lr_biases,
+            label_smoothing, whiten_bias_epochs,
+            tta_level
+        )
 
-                model.zero_grad()
-                log_probs = model(images)
-                loss = self.criterion(log_probs, labels)
-                loss.backward()
-                optimizer.step()
-
-                if self.args.verbose and (batch_idx % 10 == 0):
-                    print('| Global Round : {} | Local Epoch : {} | [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
-                        global_round, iter, batch_idx * len(images),
-                        len(self.trainloader.dataset),
-                        100. * batch_idx / len(self.trainloader), loss.item()))
-                self.logger.add_scalar('loss', loss.item())
-                batch_loss.append(loss.item())
-
-            epoch_loss.append(sum(batch_loss)/len(batch_loss))
-
-        return model.state_dict(), sum(epoch_loss) / len(epoch_loss)
+        return model.state_dict(), sum(train_loss_collect) / len(train_loss_collect)
 
     def inference(self, model):
         """ Returns the inference accuracy and loss.
@@ -123,30 +123,26 @@ class LocalUpdate(object):
 
 
 class ByzantineLocalUpdate(LocalUpdate):
-    def update_weights(self, model, global_round):
+    def update_weights(self, model, epochs=9.9, global_round=None):
         w_t = copy.deepcopy(model.state_dict())
         for key in w_t.keys():
             w_t[key] = torch.zeros_like(w_t[key])
         return w_t, None
 
 
-def test_inference(args, model, test_dataset):
+def test_inference(args, model, test_dataset, gpu=0):
     """ Returns the test accuracy and loss.
     """
+    device = torch.device(gpu)
 
     model.eval()
     loss, total, correct = 0.0, 0.0, 0.0
 
-    device = torch.device('cuda' if (
-        args.gpu != None and torch.cuda.is_available()) else 'cpu')
-    if device.type == 'cuda':
-        torch.cuda.set_device(args.gpu)
-
     # criterion = nn.NLLLoss().to(device)
     criterion = nn.CrossEntropyLoss().to(device)
 
-    testloader = DataLoader(
-        test_dataset, batch_size=args.local_bs, num_workers=4, shuffle=False)
+    testloader = CifarLoader(
+        test_dataset, batch_size=args.local_bs)
 
     for batch_idx, (images, labels) in enumerate(testloader):
         images, labels = images.to(device), labels.to(device)

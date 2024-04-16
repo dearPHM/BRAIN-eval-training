3ec01a935539206a647910f4535ea788b2da6dcb
lukepark327 <lukepark327@gmail.com>
2024-04-16 15:37:43 +0900

    chore: time test settings

--- time.sh
+++ time.sh
@@ -1,3 +1,6 @@
+# rm -rf results
 mkdir results
 
-python src/brain_main.py --iid=1 --epochs=50 --frac=0.1 --stale=4 --diff=0.0 --window=4 --threshold=0.0 --verbose=0
+for i in $(seq 1 100); do
+    python src/brain_main.py --iid=1 --epochs=50 --frac=0.1 --stale=4 --diff=0.0 --window=4 --threshold=0.0 --verbose=0
+done
